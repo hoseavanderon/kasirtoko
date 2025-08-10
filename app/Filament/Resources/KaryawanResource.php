ab6355043bd773174dbcc2b78a6f23687fa1e23a
hoseavanderson <hosea,vanderson2003@gmail.com>
2025-08-10 22:25:27 +0800

    buat halaman karyawan admin

---
+++ app/Filament/Resources/KaryawanResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\KaryawanResource\Pages;
+use App\Filament\Resources\KaryawanResource\RelationManagers;
+use App\Models\Karyawan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class KaryawanResource extends Resource
+{
+    protected static ?string $model = Karyawan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+    protected static ?string $navigationLabel = 'Karyawan';
+    protected static ?string $pluralModelLabel = 'Karyawan';
+    protected static ?string $modelLabel = 'Karyawan';
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nama')
+                    ->label('Nama')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\DatePicker::make('bekerja_sejak')
+                    ->label('Bekerja Sejak')
+                    ->required(),
+
+                Forms\Components\FileUpload::make('foto')
+                    ->label('Foto')
+                    ->image()
+                    ->directory('karyawan-foto')
+                    ->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nama')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('bekerja_sejak')
+                    ->label('Bekerja Sejak')
+                    ->date()
+                    ->sortable(),
+
+                Tables\Columns\ImageColumn::make('foto')
+                    ->label('Foto')
+                    ->circular(),
+                
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListKaryawans::route('/'),
+            'create' => Pages\CreateKaryawan::route('/create'),
+            'edit' => Pages\EditKaryawan::route('/{record}/edit'),
+        ];
+    }
+}
